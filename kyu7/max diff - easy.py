dd2b974e782d5c4cc9b1b97a46c8921df296ff09
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-07-01 13:04:30 +0300

    Create max diff - easy.py

---
+++ kyu7/max diff - easy.py
@@ -0,0 +1,19 @@
+"""You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7"""
+def max_diff(lst):
+    if len(lst)==0:
+        return(0)
+    else:
+        minumum = lst[0]
+        max = lst[0]
+        for i in range(0,len(lst)):
+            if minumum > lst[i]:
+                minumum = lst[i]
+            if max < lst[i]:
+                max = lst[i]
+        return(max - minumum)
