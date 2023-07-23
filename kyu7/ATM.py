ff51f64f0e5eda1ad1f88de92081ccd20c030766
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-07-23 15:31:28 +0300

    Update ATM.py

--- kyu7/ATM.py
+++ kyu7/ATM.py
@@ -3,3 +3,26 @@
 You have to write the ATM's function that determines the minimal number of banknotes needed to honor a withdrawal of n dollars, with 1 <= n <= 1500.
 
 Return that number, or -1 if it is impossible."""
+def solve(n):
+    if n % 10 != 0:
+        return -1
+    else:
+        k = []
+        m = n
+        if m // 500 > 0:
+            k.append(m // 500)
+            m = m - (m // 500) * 500
+        if m // 200 > 0:
+            k.append(m // 200)
+            m = m - (m // 200) * 200
+        if m // 100 > 0:
+            k.append(m // 100)
+            m = m - (m // 100) * 100
+        if m // 50 > 0:
+            k.append(m // 50)
+            m = m - (m // 50) * 50
+        if m // 20 > 0:
+            k.append(m // 20)
+            m = m - (m // 20) * 20
+        k.append(m // 10)
+    return sum(k)
