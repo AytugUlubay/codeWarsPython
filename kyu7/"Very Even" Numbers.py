9dfb799f3844b58c543146a9a67285d4d5bef788
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-07-26 19:41:54 +0300

    Create  "Very Even" Numbers.py

---
+++ kyu7/"Very Even" Numbers.py
@@ -0,0 +1,25 @@
+"""Task
+Write a function that returns true if the number is a "Very Even" number.
+
+If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+
+Examples
+number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+
+number = 5 => returns false
+
+number = 841 => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+Note: The numbers will always be 0 or positive integers!"""
+def is_very_even_number(n):
+    l=list(str(n))
+    while len(l)>1:
+        k= str(sum(int(i)for i in l))
+        l=list(k)
+    m=int(l[0])
+    if m%2==0:
+        return( True)
+    else: return(False)
