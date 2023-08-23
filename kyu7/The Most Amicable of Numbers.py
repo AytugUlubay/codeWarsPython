524af88406f1543917770696a817de90fef4168f
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-08-23 16:36:30 +0300

    Create The Most Amicable of Numbers.py

---
+++ kyu7/The Most Amicable of Numbers.py
@@ -0,0 +1,20 @@
+"""Amicable numbers are two different numbers so related that the sum of the proper divisors of each is equal to the other number. (A proper divisor of a number is a positive factor of that number other than the number itself. For example, the proper divisors of 6 are 1, 2, and 3.)
+
+For example, the smallest pair of amicable numbers is (220, 284); for the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110, of which the sum is 284; and the proper divisors of 284 are 1, 2, 4, 71 and 142, of which the sum is 220.
+
+Derive function amicableNumbers(num1, num2) which returns true/True if pair num1 num2 are amicable, false/False if not.
+
+See more at https://en.wikipedia.org/wiki/Amicable_numbers"""
+def divi(n):
+    total = 0
+    for i in range(1, n):
+        if n % i == 0:
+            total += i
+    return total
+def amicable_numbers(n1, n2):
+    total1 = divi(n1)
+    total2 = divi(n2)
+    
+    if total1 == n2 and total2 == n1:
+        return True
+    return False
