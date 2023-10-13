0711040b9f87088e3faa9f6904804b207cfbd594
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-10-13 16:34:27 +0300

    Create Exclamation marks series #2: Remove all exclamation marks from the end of sentence.py

---
+++ kyu8/Exclamation marks series #2: Remove all exclamation marks from the end of sentence.py
@@ -0,0 +1,15 @@
+"""Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+"""
+def remove(s):
+    while s.endswith("!"):
+        s = s[:-1]
+    return s
