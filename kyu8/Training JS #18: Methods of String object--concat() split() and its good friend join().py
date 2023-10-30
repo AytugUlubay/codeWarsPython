80a7a7d9ab669c40e2b3ed6888b7f48d4852c481
AytugUlubay <137622779+AytugUlubay@users.noreply.github.com>
2023-10-30 23:29:12 +0300

    Create Training JS #18: Methods of String object--concat() split() and its good friend join().py

---
+++ kyu8/Training JS #18: Methods of String object--concat() split() and its good friend join().py
@@ -0,0 +1,20 @@
+"""Task
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+For example:
+
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+"""
+def split_and_merge(string_, separator):
+    l= string_.split(" ")
+    s=""
+    for i,e in enumerate(l):
+        s= s + f"{ separator}".join(e)
+        if i != len(l)-1:
+            s = s + " "
+    return s
